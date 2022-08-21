6676622b1d7c12d91b3bc86b584d04b76a241e12
PeterKellett <peterwkellett@gmail.com>
2022-08-21 17:50:54 +0000

    Project set up

--- calcount/settings.py
+++ calcount/settings.py
@@ -20,10 +20,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-8x=1w72(2d@uwhrhkq9qfj+r62m0c(&(a%5rht*xowubkeonda'
+SECRET_KEY = os.environ.get('CALCOUNT_SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['gitpod.io',
                  'localhost']
